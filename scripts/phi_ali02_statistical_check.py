80292cc9cc50fc075780a00d3c97e111f4b1e294
Syed Muhammad Ali Shah <smas1c15@ecs.soton.ac.uk>
2016-02-18 00:36:02 +0000

    deeply understanding the data, worth seeing

---
+++ scripts/phi_ali02_statistical_check.py
@@ -0,0 +1,132 @@
+
+# coding: utf-8
+
+# In[2]:
+
+import numpy as np #linear algebra
+import pandas as pd #data processing
+import io
+
+training_data = pd.read_csv("train.csv", encoding="ISO-8859-1") #read train.csv
+testing_data = pd.read_csv("test.csv", encoding="ISO-8859-1") #read test.csv
+attribute_data = pd.read_csv("attributes.csv") #read attributes.csv
+descriptions = pd.read_csv("product_descriptions.csv") #read product_descriptions.csv
+
+training_data = pd.merge(training_data, descriptions, on="product_uid", how="left") #merge data
+
+product_counts = pd.DataFrame(pd.Series(training_data.groupby(["product_uid"]).size(), name="product_count")) #count
+training_data = pd.merge(training_data, product_counts, left_on="product_uid", right_index=True, how="left")
+
+brand_names = attribute_data[attribute_data.name == "MFG Brand Name"][["product_uid", "value"]].rename(columns={"value": "brand_name"})
+training_data = pd.merge(training_data, brand_names, on="product_uid", how="left") #merge brand names
+training_data.brand_name.fillna("Unknown", inplace=True)
+
+
+# In[3]:
+
+print(str(training_data.info()))
+print(str(training_data.describe()))
+training_data[:10]
+
+
+# In[4]:
+
+print(attribute_data.name.value_counts())
+print(attribute_data.value[attribute_data.name == "Indoor/Outdoor"].value_counts())
+
+
+# In[5]:
+
+training_data["id_bins"] = pd.cut(training_data.id, 20, labels=False)
+print(training_data.corr(method="spearman"))
+training_data.describe()
+
+
+# In[6]:
+
+training_data.relevance.hist()
+training_data.relevance.value_counts()
+
+
+# In[8]:
+
+get_ipython().magic('matplotlib inline')
+(descriptions.product_description.str.len() / 5).hist(bins=30)
+
+
+# In[9]:
+
+(training_data.product_title.str.len() / 5).hist(bins=30)
+
+
+# In[10]:
+
+(training_data.search_term.str.len() / 5.).hist(bins=30)
+(training_data.search_term.str.count("\\s+") + 1).hist(bins=30)
+
+
+# In[11]:
+
+testing_data.product_uid.value_counts()
+
+
+# In[12]:
+
+training_products = training_data.product_uid.value_counts()
+testing_products = testing_data.product_uid.value_counts()
+training_norm = np.sqrt((training_products ** 2).sum())
+testing_norm = np.sqrt((testing_products ** 2).sum())
+product_uid_cos = (training_products * testing_products).sum() / (training_norm * testing_norm)
+print("Product distribution cosine:", product_uid_cos)
+
+
+# In[13]:
+
+import collections
+
+chars = collections.Counter()
+for title in training_data.product_title:
+    chars.update(title.lower())
+total = sum(chars.values())
+
+print("Title char counts")
+for c, count in chars.most_common(30):
+    print("0x{:02x} {}: {:.1f}%".format(ord(c),  c, 100. * count / total))
+    
+words = collections.Counter()
+for title in training_data.search_term:
+    words.update(title.lower().split())
+
+total = sum(words.values())
+print("Search word counts")
+for word, count in words.most_common(200):
+    print("{}: {:.1f}% ({:,})".format(word, 100. * count / total, count))
+
+
+# In[14]:
+
+print("Indoor/outdoor", training_data.search_term.str.contains("indoor|outdoor|interior|exterior", case=False).value_counts())
+print("Contains numbers", training_data.search_term.str.contains("\\d", case=False).value_counts())
+
+
+# In[15]:
+
+def summarize_values(name, values):
+    values.fillna("", inplace=True)
+    counts = collections.Counter()
+    for value in values:
+        counts[value.lower()] += 1
+    
+    total = sum(counts.values())
+    print("{} counts ({:,} values)".format(name, total))
+    for word, count in counts.most_common(20):
+        print("{}: {:.1f}% ({:,})".format(word, 100. * count / total, count))
+
+for attribute_name in ["Color Family", "Color/Finish", "Material", "MFG Brand Name", "Indoor/Outdoor", "Commercial / Residential"]:
+    summarize_values("\n" + attribute_name, attribute_data[attribute_data.name == attribute_name].value)
+
+
+# In[ ]:
+
+
+
