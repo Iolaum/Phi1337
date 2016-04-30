663bc2cf1dc482994189ce630d60d757298f913a
Antonios Andronis <aa3e15@soton.ac.uk>
2016-04-30 14:16:35 +0100

    applied tfidf

--- scripts/preprocessing/linear_reg.py
+++ scripts/preprocessing/linear_reg.py
@@ -5,44 +5,78 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-
-score_df = pd.read_pickle('../../dataset/score_df.pickle')
-training_set = np.array(score_df)
-
-X = training_set[:, range(1, 4)]
-Y = training_set[:, 4]
-
-# # Debug
-# print(X)
-# print(Y)
-# print(X.shape)
-# print(Y.shape)
-
-xtr, xts, ytr, yts = train_test_split(X, Y, test_size=0.2, random_state=13)
-
-# # Debug
-# print(xtr.shape)
-# print(xts.shape)
-# print(ytr.shape)
-# print(yts.shape)
-
-lin_model = LinearRegression()
-
-# from sklearn.svm import SVR
-# lin_model = SVR()
-# 0.506153191762
-# 0.506015067785
+from sklearn.svm import SVR
 
 
-lin_model.fit(xtr, ytr)
+def regression(regression_type, use_tfidf, debug=False):
+    score_df = pd.read_pickle('../../dataset/score_df.pickle')
+    rate_range = range(0, 4)
+    y_col = 4
 
-# Check for overfitting. Predicted the relevance for the training data.
-ytr_pred = lin_model.predict(xtr)
-ytr_error = sqrt(mean_squared_error(ytr_pred, ytr))
+    if use_tfidf:
+        # tfidf score dataframe
+        print("Running Regression with TFIDF score dataframe")
+        score_df = pd.read_pickle('../../dataset/score_df_tfidf.pickle')
 
-# Predicted the relevance for the test data.
-yts_pred = lin_model.predict(xts)
-yts_error = sqrt(mean_squared_error(yts_pred, yts))
+        rate_range = range(0, 3)
+        y_col = 3
 
-print(ytr_error)
-print(yts_error)
+    training_set = np.array(score_df)
+    # # Debug
+    # print(training_set)
+
+    X = training_set[:, rate_range]
+    Y = training_set[:, y_col]
+
+    # Debug
+
+    if debug:
+        print(score_df)
+        print(X)
+        print(Y)
+        print(X.shape)
+        print(Y.shape)
+
+    xtr, xts, ytr, yts = train_test_split(X, Y, test_size=0.2, random_state=13)
+
+    # Debug
+    if debug:
+        print(xtr.shape)
+        print(xts.shape)
+        print(ytr.shape)
+        print(yts.shape)
+
+    if regression_type == 'linear':
+        print("Regression Type - Linear")
+        lin_model = LinearRegression()
+    else:
+        print("Regression Type - SVR(RBF)")
+        lin_model = SVR()
+
+    lin_model.fit(xtr, ytr)
+
+    # Check for overfitting. Predicted the relevance for the training data.
+    print("")
+    print("Check for overfitting")
+    ytr_pred = lin_model.predict(xtr)
+    ytr_error = sqrt(mean_squared_error(ytr_pred, ytr))
+    print(ytr_error)
+
+    print("")
+
+    # Predicted the relevance for the test data.
+    print("Predict the relevance for the test data")
+    yts_pred = lin_model.predict(xts)
+    yts_error = sqrt(mean_squared_error(yts_pred, yts))
+
+    print(yts_error)
+
+
+if __name__ == "__main__":
+    # Change between SVR or Linear
+    regression_type = 'svr'
+
+    # Change to use tfidf scores
+    use_tfidf = True
+
+    regression(regression_type, use_tfidf, debug=True)
---
+++ scripts/preprocessing/tf_idf.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import numpy as np
+
+from word_count_evaluation import clean_text, tokenize_and_stem
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.metrics.pairwise import polynomial_kernel
+from sklearn.metrics.pairwise import sigmoid_kernel
+from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.metrics.pairwise import laplacian_kernel
+from gensim.models.tfidfmodel import TfidfModel
+
+
+def get_kernel_types():
+    return [
+        'cosine_similarity',
+        'linear',
+        'polynomial',
+        'sigmoid',
+        'rbf',
+        'laplacian',
+    ]
+
+
+def get_terms_from_tf_idf(tfidf_vectorizer):
+    terms = tfidf_vectorizer.get_feature_names()
+
+    return terms
+
+
+def get_kernel(kernel_name):
+    options = {
+        'cosine_similarity': cosine_similarity,
+        'linear': linear_kernel,
+        'polynomial': polynomial_kernel,
+        'sigmoid': sigmoid_kernel,
+        'rbf': rbf_kernel,
+        'laplacian': laplacian_kernel
+    }
+
+    return options[kernel_name]
+
+
+def get_similarity_matrix(tfidf_matr, kernel):
+    dist = 1 - get_kernel(kernel)(tfidf_matr)
+
+    return dist
+
+
+def perform_tf_idf(debug=False):
+    bow_matrix = pd.read_pickle('../../dataset/bow_per_product.pickle')
+
+    max_features = None
+    # define vectorizer parameters
+    print("Setup TF-IDF Vectorizer")
+
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.7, max_features=max_features,
+                                       min_df=0.2, stop_words=None,
+                                       use_idf=True, tokenizer=None)
+
+    print("Perform TF-IDF on the search results -- Max features = " + str(max_features))
+    kernel_type = 'rbf'
+
+    training_data = pd.read_csv('../../dataset/train.csv', encoding='ISO-8859-1')
+
+    # # debug prints
+    #     print("Bag of words matrix: ")
+    #     print(bow_matrix)
+    #     print("")
+    #     print("Training Data: ")
+    #     print(training_data)
+
+    score_df = pd.DataFrame(
+        columns=['title_rate', 'desc_rate', 'attr_rate', 'relevance'],
+        index=training_data['id'].tolist()
+    )
+
+    counter = 0
+    for isearch in training_data.iterrows():
+        search_term_tokens = tokenize_and_stem(clean_text(isearch[1].search_term))
+
+        # # debug
+        # print search_term_set
+
+        # get p_id, search_id and relevance from tr_data
+        p_id = isearch[1].product_uid
+        relevance = isearch[1].relevance
+        search_id = isearch[1].id
+
+        test_matrix = [
+            " ".join(search_term_tokens),
+            " ".join(bow_matrix.ix[np.int64(p_id), 'title']),
+            " ".join(bow_matrix.ix[np.int64(p_id), 'description']),
+            " ".join(bow_matrix.ix[np.int64(p_id), 'attributes']),
+        ]
+
+        tfidf_matrix = tfidf_vectorizer.fit_transform(test_matrix)  # fit the vectorizer to books
+
+        # Run all kernels for debug reasons (see print below)
+        #
+        # for kernel_type in get_kernel_types():
+        #     print("Calculate similarity with - " + kernel_type + " kernel")
+        #     sim_matrix = get_similarity_matrix(tfidf_matrix, kernel_type)[0]
+        #     print(sim_matrix)
+        # break
+
+        # # Debug
+        # print("Calculate similarity with - " + kernel_type + " kernel")
+
+        sim_matrix = get_similarity_matrix(tfidf_matrix, kernel_type)[0]
+        title_score = sim_matrix[1]
+        desc_score = sim_matrix[2]
+        attr_score = sim_matrix[3]
+
+        # # Debug
+        # print(sim_matrix)
+        # print("Title score - " + str(title_score))
+        # print("Desc score - " + str(desc_score))
+        # print("Attrs score - " + str(attr_score))
+
+        score_row = {
+            'relevance': relevance,
+            'title_rate': title_score,
+            'desc_rate': desc_score,
+            'attr_rate': attr_score,
+        }
+
+        score_df.loc[search_id] = pd.Series(score_row)
+
+        counter += 1
+
+        if (counter is not 0 and counter % 1000 == 0):
+            print(str(counter) + " searches processed")
+        # # Stop execution for debug reasons
+        # if counter == 1000:
+        #     break
+
+    score_df.to_pickle('../../dataset/score_df_tfidf.pickle')
+
+    if debug:
+        print(score_df)
+
+    print("Score Dataframe succesfully saved!")
+    return None
+
+
+if __name__ == "__main__":
+    perform_tf_idf(debug=True)
+
+
+# Calculate similarity with - cosine_similarity kernel
+# [[  0.00000000e+00   1.00000000e+00   1.00000000e+00   1.00000000e+00]
+#  [  1.00000000e+00   1.00000000e+00   1.00000000e+00   1.00000000e+00]
+#  [  1.00000000e+00   1.00000000e+00  -2.22044605e-16   5.16654647e-01]
+#  [  1.00000000e+00   1.00000000e+00   5.16654647e-01   4.44089210e-16]]
+
+# Calculate similarity with - linear kernel
+# [[  0.00000000e+00   1.00000000e+00   1.00000000e+00   1.00000000e+00]
+#  [  1.00000000e+00   1.00000000e+00   1.00000000e+00   1.00000000e+00]
+#  [  1.00000000e+00   1.00000000e+00  -6.66133815e-16   5.16654647e-01]
+#  [  1.00000000e+00   1.00000000e+00   5.16654647e-01  -1.33226763e-15]]
+
+# Calculate similarity with - polynomial kernel
+# [[-0.03946922  0.          0.          0.        ]
+#  [ 0.          0.          0.          0.        ]
+#  [ 0.          0.         -0.03946922 -0.01895009]
+#  [ 0.          0.         -0.01895009 -0.03946922]]
+
+# Calculate similarity with - sigmoid kernel
+# [[ 0.23300535  0.23840584  0.23840584  0.23840584]
+#  [ 0.23840584  0.23840584  0.23840584  0.23840584]
+#  [ 0.23840584  0.23840584  0.23300535  0.23578215]
+#  [ 0.23840584  0.23840584  0.23578215  0.23300535]]
+
+# Calculate similarity with - rbf kernel
+# [[ 0.          0.01290305  0.0256396   0.0256396 ]
+#  [ 0.01290305  0.          0.01290305  0.01290305]
+#  [ 0.0256396   0.01290305  0.          0.01332996]
+#  [ 0.0256396   0.01290305  0.01332996  0.        ]]
+
+# Calculate similarity with - laplacian kernel
+# [[ 0.          0.01290305  0.10028499  0.07684025]
+#  [ 0.01290305  0.          0.08852418  0.06477297]
+#  [ 0.10028499  0.08852418  0.          0.09156234]
+#  [ 0.07684025  0.06477297  0.09156234  0.        ]]
+
